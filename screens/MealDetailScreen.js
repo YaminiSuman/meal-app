7ac9cc687ac848694c0ee770feeca0de4db40811
YaminiSuman <me.yamini.01@gmail.com>
2022-10-19 10:46:51 +0100

    add icon button on header right

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,37 +1,55 @@
 import { View, Text, Image, StyleSheet, ScrollView } from "react-native";
-
+import { useLayoutEffect } from "react";
 import List from "../components/MealDetail/List";
 import Subtitle from "../components/MealDetail/Subtitle";
 import MealDetails from "../components/MealDetails.js";
 import { MEALS } from "../data/dummy-data";
-
-function MealDetailScreen({ route }) {
+import IconButton from "../components/IconButton";
+function MealDetailScreen({ route, navigation }) {
   const mealId = route.params.mealId;
 
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
-    return (
-      <View style={styles.container}>
-        <ScrollView style={styles.rootContainer}>
-          <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
-          <Text style={styles.title}>{selectedMeal.title}</Text>
-          <MealDetails
-            duration={selectedMeal.duration}
-            complexity={selectedMeal.complexity}
-            affordability={selectedMeal.affordability}
-            textStyle={styles.detailText}
+  function headerButtonPressHandler() {
+    console.log("Pressed!");
+  }
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return (
+          <IconButton
+            icon="star"
+            color="white"
+            onPress={headerButtonPressHandler}
           />
-          <View style={styles.listOuterContainer}>
-            <View style={styles.listContainer}>
-              <Subtitle>Ingredients</Subtitle>
-              <List data={selectedMeal.ingredients} />
-              <Subtitle>Steps</Subtitle>
-              <List data={selectedMeal.steps} />
-            </View>
+        );
+      },
+    });
+  }, [navigation, headerButtonPressHandler]);
+
+  return (
+    <View style={styles.container}>
+      <ScrollView style={styles.rootContainer}>
+        <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
+        <Text style={styles.title}>{selectedMeal.title}</Text>
+        <MealDetails
+          duration={selectedMeal.duration}
+          complexity={selectedMeal.complexity}
+          affordability={selectedMeal.affordability}
+          textStyle={styles.detailText}
+        />
+        <View style={styles.listOuterContainer}>
+          <View style={styles.listContainer}>
+            <Subtitle>Ingredients</Subtitle>
+            <List data={selectedMeal.ingredients} />
+            <Subtitle>Steps</Subtitle>
+            <List data={selectedMeal.steps} />
           </View>
-        </ScrollView>
-      </View>
-    );
+        </View>
+      </ScrollView>
+    </View>
+  );
 }
 
 export default MealDetailScreen;
@@ -42,7 +60,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#3f2f25",
   },
   rootContainer: {
-   marginBottom:32
+    marginBottom: 32,
   },
   image: {
     width: "100%",
